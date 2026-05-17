b2a8a0b75794971ab792556dcf6423b1034b23eb
mr-nay <106431039+mr-nay@users.noreply.github.com>
2026-05-18 02:55:57 +0700

    Update api.js

--- api.js
+++ api.js
@@ -8,12 +8,32 @@ const API_BASE_URL = 'https://api.jejaring.cc/videos.php';
 
 // Sample video IDs for homepage (since no list endpoint is specified)
 const VIDEO_IDS = [
-    'sjhojyi3q8', 'aB2c4dEfGh', 'xY9z8wVuTs', 'mN3o4pQrSt',
-    'kL5j6iHgFe', 'dC7b8aZyXw', 'vU1t2sRqPo', 'nM3l4kJiHg',
-    'fE5d6cBaZy', 'xW7v8uTsRq', 'pO9n0mLkJi', 'hG1f2eDbCa',
-    'zY3x4wVuTs', 'rQ5p6oNmLk', 'jI7h8gFeDc', 'bA9z0yXwVu',
-    'tS1r2qPoNm', 'lK3j4iHgFe', 'dC5b6aZyXw', 'vU7t8sRqPo',
-    'nM9l0kJiHg', 'fE1d2cBaZy', 'xW3v4uTsRq', 'pO5n6mLkJi'
+  '690a489gl4',
+  'MJ21yWAPAu',
+  'ub0dg1buk6',
+  '4Qvl2tym6O',
+  'JeQatjNAMX',
+  'Vm1xQGvotV',
+  'B2WlfZ9Cvi',
+  'r3zlgtwiwe',
+  'sjhojyi3q8',
+  '3s14dza788',
+  '4a307e6q9j',
+  'cator0dwxs',
+  'gjvdw0io39',
+  '6eq2e9l15g',
+  '8fv7p8r6c8',
+  '0928n5f6xm',
+  '7z53i01616',
+  'w7iff04991',
+  'a829vo0713',
+  '7b23egb99f',
+  '4m2k97n5i3',
+  '236s2sr938',
+  'oy2foe237b',
+  '2aqpzgbut9',
+  '9h5y36dh94',
+  'fp0h5uusmo'
 ];
 
 /**
